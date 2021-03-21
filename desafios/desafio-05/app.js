3bfbe3442e3e53c619aab4be608ede51362226f7
Carlos Roberto <crpu4a@gmail.com>
2021-03-21 18:37:25 -0300

    Atualização dos arquivos do curso.

---
+++ desafios/desafio-05/app.js
@@ -0,0 +1,23 @@
+new Vue({
+	el: '#desafio',
+	data: {
+		exibirConteudo: true,
+		array: ['Pedro', 'Bia', 'Ana', 'Rebeca'],
+		livro: {
+			titulo: 'O Senhor dos Anéis',
+			autor: 'J.R.R. Tolkiens',
+			volume: '3'
+		},
+		aluno: {
+			id: 10,
+			nome: 'Maria',
+			notas: [7.67, 8.33, 6.98, 9.21]
+		},
+		marcas: ['nike', 'pulma', 'renner', 'google', 'baú do rock']
+	},
+	computed: {
+		ehArray(item){
+			return Array.isArray(item);
+		}
+	}
+});
