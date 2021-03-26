5da102be71bc3a30892c3ecf6c60525c4bd01b7a
Carlos Roberto <crpu4a@gmail.com>
2021-03-26 12:24:52 -0300

    Arquivos do componente-desafio

---
+++ componentes/componente-desafio/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <app-menu />
+    <app-header />
+    <app-carousel />
+    <app-footer />
+
+  </v-app>
+</template>
+
+<script>
+import Menu from '@/components/template/Menu.vue'
+import Header from '@/components/template/Header.vue'
+import Carousel from '@/components/widgets/Carousel.vue'
+import Footer from '@/components/template/Footer.vue'
+
+
+export default {
+  components: {
+    'app-menu': Menu,
+    'app-header': Header,
+    'app-carousel': Carousel,
+    'app-footer': Footer,
+  }
+}
+</script>
