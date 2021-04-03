713d82b25a2b89b1c78bda4d8a21bf6be61a149f
Carlos Roberto <crpu4a@gmail.com>
2021-04-03 13:04:17 -0300

    Um pouco sobre Slots dinâmicos.

--- componentes-comunicacao/citacoes/src/components/Citacao.vue
+++ componentes-comunicacao/citacoes/src/components/Citacao.vue
@@ -1,22 +1,30 @@
 <template>
-    <div class="citacao">
-        <slot name="autor"></slot>
-        <slot name="texto"></slot>
-        <slot name="fonte"></slot>
-    </div>
+  <div class="citacao">
+    <slot name="autor"></slot>
+    <slot name="texto"></slot>
+    <slot name="fonte"></slot>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  // Métodos que executam quando ativados e desativados os componentes dinâmicos.
+  activated() {
+    // eslint-disable-next-line
+    console.log("Citação: activated");
+  },
+  deactivated() {
+    // eslint-disable-next-line
+    console.log("Citação: deactivated");
+  },
+};
 </script>
 
 <style scoped>
-    .citacao {
-        border: 1px solid #DDD;
-        background-color: rgba(0, 0, 0, .1);
-        padding: 25px;
-        max-width: 450px;
-    }
+.citacao {
+  border: 1px solid #ddd;
+  background-color: rgba(0, 0, 0, 0.1);
+  padding: 25px;
+  max-width: 450px;
+}
 </style>
--- componentes-comunicacao/citacoes/src/components/Sobre.vue
+++ componentes-comunicacao/citacoes/src/components/Sobre.vue
@@ -1,38 +1,45 @@
 <template>
-    <div>
-        <p class="nome">Citações</p>
-        <p class="versao">Versão 1.0</p>
-    </div>
+  <div>
+    <p class="nome">Citações</p>
+    <p class="versao">Versão 1.0</p>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  activated() {
+    // eslint-disable-next-line
+    console.log("Sobre: activated");
+  },
+  deactivated() {
+    // eslint-disable-next-line
+    console.log("Sobre: deactivated");
+  },
+};
 </script>
 
 <style scoped>
-    div {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        border: 1px solid #DDD;
-        height: 200px;
-        width: 200px;
-        border-radius: 100px;
-    }
+div {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #ddd;
+  height: 200px;
+  width: 200px;
+  border-radius: 100px;
+}
 
-    p {
-        margin: 0;
-    }
+p {
+  margin: 0;
+}
 
-    .nome {
-        font-size: 2.1rem;
-        font-weight: bold;
-    }
+.nome {
+  font-size: 2.1rem;
+  font-weight: bold;
+}
 
-    .versao {
-        font-size: 1.4rem;
-    }
+.versao {
+  font-size: 1.4rem;
+}
 </style>
